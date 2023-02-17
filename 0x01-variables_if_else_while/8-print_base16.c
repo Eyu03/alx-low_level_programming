54cc925513058b98e9f0c277e01811ec40ec45c5
Eyob Kurabachew <03eyobkura@gmail.com>
2023-02-17 11:12:15 -0800

    For the task Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,7 +4,7 @@
  * main -  main block
  * Description: prints all single digit number of base 10
  * starting from 0, followed by a new line.
- * Return: always 0
+ * Return: always 0 (Success)
  */
 
 int main(void)
