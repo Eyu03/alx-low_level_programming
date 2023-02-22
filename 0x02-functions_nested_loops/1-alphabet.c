83c17d0aee79699f812104bd6c8b90f945b813c1
Eyob Kurabachew <03eyobkura@gmail.com>
2023-02-22 04:57:47 -0800

    For the task I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (latter = 'a'; latter <= 'z'; latter++)
+	{
+		_putchar(latter);
+	}
+	_putchar('\n');
+}
