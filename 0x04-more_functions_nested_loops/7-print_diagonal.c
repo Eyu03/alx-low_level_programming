7542cc191516fa7b483e1aa6f4c0e9063aa8b68e
Eyob Kurabachew <03eyobkura@gmail.com>
2023-02-24 08:14:35 -0800

    For the task I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,23 @@
 #include "main.h"
+
 /**
- * print_square - draws a square using multiple terminal lines and #'s.
- * @n: the number of # to print in each line.
+ * print_diagonal - prints diagonal line n times.
+ * @n: times diagonal line is printed.
+ * Return: no return.
  */
-void print_square(int n)
+void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < n; j++)
-				_putchar('#');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
